d4f93f52b778b92404607ec80bf2e76c3ebbeced
Mazlum <mazlumsert@gmail.com>
2017-11-16 12:11:32 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,14 @@ public class Main {
     public static void main(String[] args) {
 
         ReceiverController receiver = new ReceiverController("guest", "datdb.cphbusiness.dk", "reciplist");
-        //SenderController send = new SenderController("guest","datdb.cphbusiness.dk","reciplist1");
+        SenderController send = new SenderController("guest","datdb.cphbusiness.dk","reciplist1");
 
         if (receiver.isReady()) {
             for (String msg : receiver.getMessages()) {
-                System.out.println(msg);
+              send.sendMessage(msg);
             }
             receiver.close();
-            //  send.sendMessage("Sker der joe");
-            //   send.close();
+            send.close();
         }
     }
 
